d3572a2ea78173b02a9fa2f6dc38febdb1145241
nikenine01 <151134838+nikenine01@users.noreply.github.com>
2024-10-17 00:53:20 +0700

    fix some typing

--- airflow_1/dags/data_ingestion_local.py
+++ airflow_1/dags/data_ingestion_local.py
@@ -4,9 +4,9 @@ import os
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+# from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
 
-from scripts.ingestion_script import ingestion_data
+# from scripts.ingestion_script import ingestion_data
 from scripts.gcs_script import upload_to_gcs
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
@@ -16,10 +16,10 @@ BUCKET = os.environ.get("GCP_GCS_BUCKET")
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 URL_PREFIX = "https://d37ci6vzurychx.cloudfront.net/trip-data"
 
-URL_TEMPLATE = "{{ params.URL_PREFIX }}/yellow_tripdata_{{ execution_date_strftime(\'%Y-%m\') }}.parquet"
-OUTPUT_FILE_TEMPLATE = "{{ params.AIRFLOW_HOME}}/output_{{ execution_date_strftime(\'%Y-%m\') }}.parquet"
+URL_TEMPLATE = URL_PREFIX + "/yellow_tripdata_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + "/output_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
 TABLE_NAME_TEMPLATE = "yellow_taxi_{{ execution_date.strftime('%Y-%m') }}"
-GCS_OBJECT_TEMPLATE = 'raw/yellow_taxi_{{ execution_date.strftime(\'%Y-%m\') }}.parquet'
+GCS_OBJECT_TEMPLATE = "raw/yellow_taxi_{{ execution_date.strftime(\'%Y-%m\') }}.parquet"
 
 # Default arguments for the DAG
 args = {
@@ -34,9 +34,9 @@ dag = DAG(
     default_args=args,
     description="Downloading and processing NYC taxi data",
     schedule_interval="0 6 2 * *",  # Run on the 2nd of every month at 6:00 AM
-    start_date=datetime(2020, 1, 1),
+    start_date=datetime(2024, 1, 1),
     catchup=True,
-    max_active_run=3
+    max_active_runs=3
 )
 
 
@@ -48,15 +48,15 @@ download_parquet_task = BashOperator(
     dag = dag
 )
 
-ingestion_task = PythonOperator(
-    task_id="ingesting_nyc_yellow_taxi_data",
-    python_callable=ingestion_data,
-    op_kwargs=dict(
-        table_name=TABLE_NAME_TEMPLATE,
-        parquet_file=OUTPUT_FILE_TEMPLATE
-    ),
-    dag = dag
-)
+# ingestion_task = PythonOperator(
+#     task_id="ingesting_nyc_yellow_taxi_data",
+#     python_callable=ingestion_data,
+#     op_kwargs=dict(
+#         table_name=TABLE_NAME_TEMPLATE,
+#         parquet_file=OUTPUT_FILE_TEMPLATE
+#     ),
+#     dag = dag
+# )
 
 local_to_gcs_task = PythonOperator(
     task_id="local_to_gcs_task",
@@ -68,3 +68,10 @@ local_to_gcs_task = PythonOperator(
     ),
     dag = dag
 )
+
+remove_files_task = BashOperator(
+    task_id="remove_file_task",
+    bash_command=f"rm {OUTPUT_FILE_TEMPLATE}"
+)
+
+download_parquet_task >> local_to_gcs_task >> remove_files_task
